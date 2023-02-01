0f07d9b86a237612190940d651f9359a2cb81e6c
petrus <pshai671@gmail.com>
2023-02-01 08:26:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using oop_of_shapes;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Square square = new Square("Square", "Four sides", "Four equal sides");
+        Square rectangle = new Square("Rectangle", "Four sides", "2 sets of 2 equal sides");
+        Square triangle = new Square("Triangle", "Three sides", "It can have  2 or 3 equal sides");
+        Square pentagon = new Square("Pentagon", "Five sides", "5 sides (can be equal or unequal)");
+
+
+        Console.WriteLine(square.Name +" "+square.Numberofsides+" "+square.Properties);
+        Console.WriteLine(rectangle.Name + " " + rectangle.Numberofsides + " " + rectangle.Properties);
+        Console.WriteLine(triangle.Name + " " + triangle.Numberofsides + " " + triangle.Properties);
+        Console.WriteLine(pentagon.Name + " " + triangle.Numberofsides + " " + triangle.Properties);
+    }
+}
---
+++ Shape.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_of_shapes
+{
+    class Shape
+    {
+        public string Nameofshape;
+        public string Numberofsides;
+        public string Properties;
+
+        class Square : Shape
+        {
+            public Square(string nameofshape, string numberofsides, string properties)
+            {
+                Nameofshape = nameofshape;
+                Numberofsides = numberofsides;
+                Properties = properties;
+            }
+        }
+    }
+}
+        
